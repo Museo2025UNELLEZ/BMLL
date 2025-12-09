c503aeeca577a851559a0e2ea86f2d71aec18a5c
Lu1s2556 <luiserojasp06@gmail.com>
2025-12-09 14:06:32 -0400

    Primera version java
    Creacion de proyecto, en MVC y bocetos de login y menu principal

---
+++ src/main/java/com/museodelosllanos/bmll/BMLL.java
@@ -0,0 +1,12 @@
+package com.museodelosllanos.bmll;
+
+/**
+ *
+ * @author luise
+ */
+public class BMLL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
